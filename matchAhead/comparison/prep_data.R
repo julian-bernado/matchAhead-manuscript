21be67a948df85eef313a61f4d195a9b463bcbd7
Julian Bernado <bernado@umich.edu>
2025-08-20 12:01:51 -0700

    size management

--- matchAhead/comparison/prep_data.R
+++ matchAhead/comparison/prep_data.R
@@ -8,7 +8,8 @@ prep_data <- function(path, S, proportion_treated = 268/3605){
   df <- read_csv(path) %>% dplyr::select(-any_of(c("studentid_state_enroll")))
   schools <- df %>% pull(schoolid_state_enroll_p0) %>% unique()
   num_schools <- length(schools)
-  selected_schools <- sample(schools, size = min(S, num_schools))
+  S = min(S, num_schools)
+  selected_schools <- sample(schools, size = S)
   df <- df %>%
     filter(schoolid_state_enroll_p0 %in% selected_schools) %>%
     assign_treatment(Nt = max(1, floor(S * proportion_treated)), grouping = "schoolid_state_enroll_p0")
