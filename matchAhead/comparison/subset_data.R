66353c377d38b5258c0a1f0dce5b3dd94511b179
Julian Bernado <bernado@umich.edu>
2025-09-29 12:41:01 -0700

    actually writing

--- matchAhead/comparison/subset_data.R
+++ matchAhead/comparison/subset_data.R
@@ -6,9 +6,11 @@ subset_data <- function(grade, subject, N = 250) {
   unique_schools <- df |> pull(schoolid_state_enroll_p0) |> unique()
   subset_schools <- sample(unique_schools, N)
   sub_df <- df |> filter(schoolid_state_enroll_p0 %in% subset_schools)
-  print(length(unique_schools))
-  print(nrow(df))
-  print(nrow(sub_df))
+  write_csv(paste0("data/2019sub_", grade, "_", subject, "_df.csv"))
+  df2 <- read_csv(sub_df, paste0("data/2022_", grade, "_", subject, "_df.csv"))
+  sub_df2 <- df2 |> filter(schoolid_state_enroll_p0 %in% subset_schools)
+  write_csv(sub_df2, paste0("data/2022sub_", grade, "_", subject, "_df.csv"))
+
 }
 
 grades <- c(3, 4, 5)
